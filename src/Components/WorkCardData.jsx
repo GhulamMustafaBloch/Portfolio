72accab961cb9ff45d122340b2061d652c706613
GhulamMustafaBloch <ghulammustafabloch0302666@gmail.com>
2023-05-30 22:18:19 +0500

    adding

--- src/Components/WorkCardData.jsx
+++ src/Components/WorkCardData.jsx
@@ -10,127 +10,149 @@ import pro9 from "../assets/[GetPaidStock.com]-6474c674e616c.jpg";
 import pro10 from "../assets/[GetPaidStock.com]-6474c701c89b2.jpg";
 import pro11 from "../assets/GetPaidStock.com-6474c1778152b.jpg";
 import pro12 from "../assets/GetPaidStock.com-6474c230520ed.jpg";
+import pro12 from "../assets/IMG_20230405_225924.jpg";
 
 const ProjectCardData = [
-    {
-        imgsrc: pro1,
-        title: "Khatme Nabuwat Website",
-        text: "thriving Facebook community and top-ranked podcast, Amy inspires aa grounded, tangible and self-affirming sense of Wow! really can do this for over 100,000 online entrepreneurs. Best-selling courses",
-        view: "https://ghulammustafabloch.github.io/website/",
-        viewText: "view",
-        viewSource: "Source"
-    },
-   {
-       imgsrc: pro2,
-       title: "Online Quran Website",
-       text: "thriving Facebook community and top-ranked podcast, Amy inspires aa grounded, tangible and self-affirming sense of Wow! really can do this for over 100,000 online entrepreneurs. Best-selling courses",
-       view: "https://ghulammustafabloch.github.io/newApp/",
-       viewText: "view",
-       viewSource: "Source"
-   },
-   {
-       imgsrc: pro3,
-       title: "Shopify Website Design",
-       text: "Podcast thriving Facebook community and top-ranked podcast, Amy inspires aa grounded, tangible and self-affirming sense of Wow! really can do this for over 100,000 online entrepreneurs. Best-selling courses",
-       view: "https://ghulammustafabloch.github.io/Shopify/",
-       viewText: "view",
-       viewSource: "Source"
-   },
-   {
-       imgsrc: pro4,
-       title: "My Portfolio",
-       text: "top-ranked thriving Facebook community and  podcast, Amy inspires a grounded, tangible and self-affirming sense of Wow! really can do this for over 100,000 online entrepreneurs. Best-selling courses",
-       view: "https://ghulammustafabloch.github.io/Myportfolio/",
-       viewText: "view",
-       viewSource: "Source"
-   },
-   {
-       imgsrc: pro5,
-       title: "Rekhta Website",
-       text: "top-ranked thriving Facebook community and  podcast, Amy inspires a grounded, tangible and self-affirming sense of Wow! really can do this for over 100,000 online entrepreneurs. Best-selling courses",
-       view: "https://ghulammustafabloch.github.io/Rekhta_Website/",
-       viewText: "view",
-       viewSource: "Source"
-   },
-   {
-       imgsrc: pro6,
-       title: "Counter",
-       text: "top-ranked thriving Facebook community and  podcast, Amy inspires a grounded, tangible and self-affirming sense of Wow! really can do this for over 100,000 online entrepreneurs. Best-selling courses",
-       view: "https://ghulammustafabloch.github.io/Counter/",
-       viewText: "view",
-       viewSource: "Source"
-   },
-   {
-       imgsrc: pro7,
-       title: "iEducate Website",
-       text: "top-ranked thriving Facebook community and  podcast, Amy inspires a grounded, tangible and self-affirming sense of Wow! really can do this for over 100,000 online entrepreneurs. Best-selling courses",
-       view: "https://ghulammustafabloch.github.io/iEducate/",
-       viewText: "view",
-       viewSource: "Source"
-   },
-   {
-       imgsrc: pro8,
-       title: "Bolly Flix Website",
-       text: "top-ranked thriving Facebook community and  podcast, Amy inspires a grounded, tangible and self-affirming sense of Wow! really can do this for over 100,000 online entrepreneurs. Best-selling courses",
-       view: "https://ghulammustafabloch.github.io/New_Bolly_flix_website/",
-       viewText: "view",
-       viewSource: "Source"
-   },
-   {
-       imgsrc: pro9,
-       title: "Tutorify Website",
-       text: "top-ranked thriving Facebook community and  podcast, Amy inspires a grounded, tangible and self-affirming sense of Wow! really can do this for over 100,000 online entrepreneurs. Best-selling courses",
-       view: "https://ghulammustafabloch.github.io/Tutorify/",
-       viewText: "view",
-       viewSource: "Source"
-   },
-   {
-       imgsrc: pro10,
-       title: "Math Game",
-       text: "top-ranked thriving Facebook community and  podcast, Amy inspires a grounded, tangible and self-affirming sense of Wow! really can do this for over 100,000 online entrepreneurs. Best-selling courses",
-       view: "https://ghulammustafabloch.github.io/math_game/",
-       viewText: "view",
-       viewSource: "Source"
-   },
-   {
-       imgsrc: pro11,
-       title: "Quran Application",
-       text: "top-ranked thriving Facebook community and  podcast, Amy inspires a grounded, tangible and self-affirming sense of Wow! really can do this for over 100,000 online entrepreneurs. Best-selling courses",
-       view: "https://ghulammustafabloch.github.io/QuranApp/",
-       viewText: "view",
-       viewSource: "Source"
-   },
-   {
-       imgsrc: pro12,
-       title: "Quran Application",
-       text: "top-ranked thriving Facebook community and  podcast, Amy inspires a grounded, tangible and self-affirming sense of Wow! really can do this for over 100,000 online entrepreneurs. Best-selling courses",
-       view: " https://ghulammustafabloch.github.io/Quran_App/",
-       viewText: "view",
-       viewSource: "Source"
-   }
-//    {
-//        imgsrc: pro1,
-//        title: "Ecommerce Website Design",
-//        text: "thriving Facebook community and top-ranked podcast, Amy inspires aa grounded, tangible and self-affirming sense of Wow! really can do this for over 100,000 online entrepreneurs. Best-selling courses",
-//        view: "https://youtu.be/masvxWZU0ec",
-//        viewText: "view",
-//        viewSource: "Source"
-//    },
-//    {
-//        imgsrc: pro2,
-//        title: "LMS/Course Website Design",
-//        text: "Podcast thriving Facebook community and top-ranked podcast, Amy inspires aa grounded, tangible and self-affirming sense of Wow! really can do this for over 100,000 online entrepreneurs. Best-selling courses",
-//        view: "https://youtu.be/masvxWZU0ec",
-//        viewText: "view",
-//        viewSource: "Source"
-//    },
-//    {
-//        imgsrc: pro3,
-//        title: "Rymo Ecommerce Design",
-//        text: "top-ranked thriving Facebook community and  podcast, Amy inspires a grounded, tangible and self-affirming sense of Wow! really can do this for over 100,000 online entrepreneurs. Best-selling courses",
-//        view: "https://youtu.be/masvxWZU0ec",
-//        viewText: "view",
-//        viewSource: "Source"
-//    }
+  {
+    imgsrc: pro1,
+    title: "Khatme Nabuwat Website",
+    text:
+      "thriving Facebook community and top-ranked podcast, Amy inspires aa grounded, tangible and self-affirming sense of Wow! really can do this for over 100,000 online entrepreneurs. Best-selling courses",
+    view: "https://ghulammustafabloch.github.io/website/",
+    viewText: "view",
+    viewSource: "Source"
+  },
+  {
+    imgsrc: pro2,
+    title: "Online Quran Website",
+    text:
+      "thriving Facebook community and top-ranked podcast, Amy inspires aa grounded, tangible and self-affirming sense of Wow! really can do this for over 100,000 online entrepreneurs. Best-selling courses",
+    view: "https://ghulammustafabloch.github.io/newApp/",
+    viewText: "view",
+    viewSource: "Source"
+  },
+  {
+    imgsrc: pro3,
+    title: "Shopify Website Design",
+    text:
+      "Podcast thriving Facebook community and top-ranked podcast, Amy inspires aa grounded, tangible and self-affirming sense of Wow! really can do this for over 100,000 online entrepreneurs. Best-selling courses",
+    view: "https://ghulammustafabloch.github.io/Shopify/",
+    viewText: "view",
+    viewSource: "Source"
+  },
+  {
+    imgsrc: pro4,
+    title: "My Portfolio",
+    text:
+      "top-ranked thriving Facebook community and  podcast, Amy inspires a grounded, tangible and self-affirming sense of Wow! really can do this for over 100,000 online entrepreneurs. Best-selling courses",
+    view: "https://ghulammustafabloch.github.io/Myportfolio/",
+    viewText: "view",
+    viewSource: "Source"
+  },
+  {
+    imgsrc: pro5,
+    title: "Rekhta Website",
+    text:
+      "top-ranked thriving Facebook community and  podcast, Amy inspires a grounded, tangible and self-affirming sense of Wow! really can do this for over 100,000 online entrepreneurs. Best-selling courses",
+    view: "https://ghulammustafabloch.github.io/Rekhta_Website/",
+    viewText: "view",
+    viewSource: "Source"
+  },
+  {
+    imgsrc: pro6,
+    title: "Counter",
+    text:
+      "top-ranked thriving Facebook community and  podcast, Amy inspires a grounded, tangible and self-affirming sense of Wow! really can do this for over 100,000 online entrepreneurs. Best-selling courses",
+    view: "https://ghulammustafabloch.github.io/Counter/",
+    viewText: "view",
+    viewSource: "Source"
+  },
+  {
+    imgsrc: pro7,
+    title: "iEducate Website",
+    text:
+      "top-ranked thriving Facebook community and  podcast, Amy inspires a grounded, tangible and self-affirming sense of Wow! really can do this for over 100,000 online entrepreneurs. Best-selling courses",
+    view: "https://ghulammustafabloch.github.io/iEducate/",
+    viewText: "view",
+    viewSource: "Source"
+  },
+  {
+    imgsrc: pro8,
+    title: "Bolly Flix Website",
+    text:
+      "top-ranked thriving Facebook community and  podcast, Amy inspires a grounded, tangible and self-affirming sense of Wow! really can do this for over 100,000 online entrepreneurs. Best-selling courses",
+    view: "https://ghulammustafabloch.github.io/New_Bolly_flix_website/",
+    viewText: "view",
+    viewSource: "Source"
+  },
+  {
+    imgsrc: pro9,
+    title: "Tutorify Website",
+    text:
+      "top-ranked thriving Facebook community and  podcast, Amy inspires a grounded, tangible and self-affirming sense of Wow! really can do this for over 100,000 online entrepreneurs. Best-selling courses",
+    view: "https://ghulammustafabloch.github.io/Tutorify/",
+    viewText: "view",
+    viewSource: "Source"
+  },
+  {
+    imgsrc: pro10,
+    title: "Math Game",
+    text:
+      "top-ranked thriving Facebook community and  podcast, Amy inspires a grounded, tangible and self-affirming sense of Wow! really can do this for over 100,000 online entrepreneurs. Best-selling courses",
+    view: "https://ghulammustafabloch.github.io/math_game/",
+    viewText: "view",
+    viewSource: "Source"
+  },
+  {
+    imgsrc: pro11,
+    title: "Quran Application",
+    text:
+      "top-ranked thriving Facebook community and  podcast, Amy inspires a grounded, tangible and self-affirming sense of Wow! really can do this for over 100,000 online entrepreneurs. Best-selling courses",
+    view: "https://ghulammustafabloch.github.io/QuranApp/",
+    viewText: "view",
+    viewSource: "Source"
+  },
+  {
+    imgsrc: pro12,
+    title: "Quran Application",
+    text:
+      "top-ranked thriving Facebook community and  podcast, Amy inspires a grounded, tangible and self-affirming sense of Wow! really can do this for over 100,000 online entrepreneurs. Best-selling courses",
+    view: " https://ghulammustafabloch.github.io/Quran_App/",
+    viewText: "view",
+    viewSource: "Source"
+  },
+  {
+    imgsrc: pro13,
+    title: "My Portfolio",
+    text:
+      "top-ranked thriving Facebook community and  podcast, Amy inspires a grounded, tangible and self-affirming sense of Wow! really can do this for over 100,000 online entrepreneurs. Best-selling courses",
+    view: " https://ghulammustafabloch.github.io/Quran_App/",
+    viewText: "view",
+    viewSource: "Source"
+  }
+  //    {
+  //        imgsrc: pro1,
+  //        title: "Ecommerce Website Design",
+  //        text: "thriving Facebook community and top-ranked podcast, Amy inspires aa grounded, tangible and self-affirming sense of Wow! really can do this for over 100,000 online entrepreneurs. Best-selling courses",
+  //        view: "https://youtu.be/masvxWZU0ec",
+  //        viewText: "view",
+  //        viewSource: "Source"
+  //    },
+  //    {
+  //        imgsrc: pro2,
+  //        title: "LMS/Course Website Design",
+  //        text: "Podcast thriving Facebook community and top-ranked podcast, Amy inspires aa grounded, tangible and self-affirming sense of Wow! really can do this for over 100,000 online entrepreneurs. Best-selling courses",
+  //        view: "https://youtu.be/masvxWZU0ec",
+  //        viewText: "view",
+  //        viewSource: "Source"
+  //    },
+  //    {
+  //        imgsrc: pro3,
+  //        title: "Rymo Ecommerce Design",
+  //        text: "top-ranked thriving Facebook community and  podcast, Amy inspires a grounded, tangible and self-affirming sense of Wow! really can do this for over 100,000 online entrepreneurs. Best-selling courses",
+  //        view: "https://youtu.be/masvxWZU0ec",
+  //        viewText: "view",
+  //        viewSource: "Source"
+  //    }
 ];
 export default ProjectCardData;
